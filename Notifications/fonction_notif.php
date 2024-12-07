34fd6c68db79bb521e418368d8088bf674f30055
angeliqux <angelique.vella@hotmail.com>
2024-12-07 15:38:21 +0100

    Add functions for generate notifications

--- Notifications/fonction_notif.php
+++ Notifications/fonction_notif.php
@@ -1,5 +1,4 @@
 <?php
-include("../Fonctions.php");
 
 $IndexNotif = [
     1 => "Vous avez une nouvelle demande d’inscription à consulter.",
@@ -30,31 +29,93 @@ $IndexNotif = [
 function Verif_notif($CodeNotif, $Id_essai, $Id_destinataire){
     //Function that verify is a notifications already exist 
     $pdo = Connexion_base();
-    $stmt =  $pdo-> prepare("SELECT DESTINATAIRE.Statut_notification FROM DESTINATAIRE NATURAL JOIN NOTIFICATION WHERE DESTINATAIRE.Id_destinataire = :id_des AND NOTIFICATION.CodeNotif = :code AND NOTIFICATION.Id_Essai = :id_E;");
+    $stmt =  $pdo-> prepare("SELECT DESTINATAIRE.Statut_notification FROM DESTINATAIRE NATURAL JOIN NOTIFICATION WHERE 
+    DESTINATAIRE.Id_destinataire = :id_des AND NOTIFICATION.CodeNotif = :code AND NOTIFICATION.Id_Essai = :id_E;");
     $stmt-> execute (['id_des'=> $Id_destinataire, 'code' => $CodeNotif, 'id_E'=> $Id_essai]);
-
-    if($stmt->rowCount() < 1 || $stmt == 'Ouvert'){
+    $reponse = $stmt->fetch();
+    if($stmt->rowCount() < 1 || $reponse[0] == 'Ouvert'){
+        Fermer_base($pdo);
         return true;
     }
     else{
-
-    Fermer_base($pdo);
-}};
+        Fermer_base($pdo);
+        return false;
+    }
+};
 
 function Generer_notif($CodeNotif, $Id_essai, $Id_destinataire){
-    Connexion_base();
-
     $exist = Verif_notif($CodeNotif, $Id_essai, $Id_destinataire);
+    if ($exist) {
+        $pdo = Connexion_base();
+        // Code to generate the notification if it doesn't exist
+        $stmt = $pdo->prepare("INSERT INTO NOTIFICATION (CodeNotif, Id_Essai, Date_Notif) VALUES (:code, :id_essai, :date)");
+        $stmt->execute(['code' => $CodeNotif, 'id_essai' => $Id_essai, 'date' => date('Y-m-d H:i:s')]);
 
+        $id_Notif = $pdo->lastInsertId(); // Get the ID of the notification
+
+        // Add the recipient to the Destinataire table
+        $stmt = $pdo->prepare("INSERT INTO DESTINATAIRE (Id_notif, Id_destinataire, Statut_notification) VALUES (:id_notif, :id_dest, 'Non ouvert')");
+        $stmt->execute(['id_notif'=> $id_Notif, 'id_dest' => $Id_destinataire]);
+
+        Fermer_base($pdo);
+        return $id_Notif;
+    }else {
+        // Notification already exists
+        return false; };
 };
 
-function Pastille_notif($id_notif){
-    Connexion_base();
+function Pastille_nombre($id_D) {
+    $pdo = Connexion_base();
+    $stmt = $pdo->prepare("SELECT COUNT(*) FROM DESTINATAIRE WHERE Id_destinataire = :id_D AND Statut_notification = 'Non Ouvert'");
+    $stmt->execute(['id_D' => $id_D]);
+    $count = $stmt->fetchColumn();
 
+    // Display the count of unread notifications
+    Fermer_base($pdo);
+    return $count;
 };
 
-function Lire_notif($Id_notif, $Id_user){
-    Connexion_base();
-    //ouvrir la notif pour tous ceux qui l'ont reçu dans le cas des médecins
+function List_Notif($Id_D) {
+    $pdo = Connexion_base();
+    $stmt = $pdo->prepare('SELECT * FROM NOTIFICATION NATURAL JOIN DESTINATAIRE WHERE DESTINATAIRE.Id_destinataire = :id_D');
+    $stmt->execute(['id_D'=> $Id_D]);
+    if ($stmt->rowCount() > 0) {
+        $reponse = $stmt->fetchAll();
+        Fermer_base($pdo);
+        return $reponse;
+    } else {
+        Fermer_base($pdo);
+        return null;
+    }
+};
+
+function Lire_notif($Id_notif, $Id_user) {
+    $pdo = Connexion_base();
+    if ($_SESSION['role'] == 'Medecin') {
+        // Mark the notification as read for all doctors
+        $stmt = $pdo->prepare("UPDATE DESTINATAIRE SET Statut_notification = 'Ouvert' WHERE Id_notif = :id_notif");
+        $stmt->execute(['id_notif' => $Id_notif]);
+    } else {
+        // Mark the notification as read for the user
+        $stmt = $pdo->prepare("UPDATE DESTINATAIRE SET Statut_notification = 'Ouvert' WHERE Id_notif = :id_notif AND Id_destinataire = :id_user");
+        $stmt->execute(['id_notif' => $Id_notif, 'id_user' => $Id_user]);
+    }
+    //if role medecein => ouvert pour tous les medecins
+    Fermer_base($pdo);
+};
+
+function Ne_plus_lire_no_notif($Id_notif, $Id_user) {
+        $pdo = Connexion_base();
+        if ($_SESSION['role'] == 'Medecin') {
+            // Mark the notification as read for all doctors
+            $stmt = $pdo->prepare("UPDATE DESTINATAIRE SET Statut_notification = 'Non Ouvert' WHERE Id_notif = :id_notif");
+            $stmt->execute(['id_notif' => $Id_notif]);
+        } else {
+            // Mark the notification as read for the user
+            $stmt = $pdo->prepare("UPDATE DESTINATAIRE SET Statut_notification = 'Non Ouvert' WHERE Id_notif = :id_notif AND Id_destinataire = :id_user");
+            $stmt->execute(['id_notif' => $Id_notif, 'id_user' => $Id_user]);
+        }
+        //if role medecein => ouvert pour tous les medecins
+        Fermer_base($pdo);
 };
 ?>
