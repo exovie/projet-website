9cd22875b2545b367d9698560469b9d695989848
angeliqux <angelique.vella@hotmail.com>
2024-12-08 15:14:04 +0100

    Link page with notifications functions

--- Entreprises.php
+++ Entreprises.php
@@ -1,3 +1,9 @@
+<?php 
+session_start();
+include 'Fonctions.php';
+include 'Notifications/fonction_notif.php';
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
   <head>
@@ -6,9 +12,11 @@
     <meta charset="'utf-8">
     <link rel="stylesheet" href="website.css">
     <link rel="stylesheet" href="navigationBar.css">
+    <link rel="stylesheet" href="Notifications/Notifications_style.css">
   </head>
-  <body><!-- Conteneur fixe en haut de la page -->
-    <div class="navbar">
+  <body>
+        <!-- Code de la barre de navigation -->
+        <div class="navbar">
         <div id="logo">
             <a href="Homepage.php">
                 <img src="Pictures/logo.png" alt="minilogo" class="minilogo">
@@ -17,26 +25,68 @@
         <a href="Essais.php" class="nav-btn">Essais Cliniques</a>
         <a href="Entreprises.php" class="nav-btn">Entreprise</a>
         <a href="Contact.php" class="nav-btn">Contact</a>
+
+        <!-- Accès à la messagerie -->
+        <?php if (isset($_SESSION['Logged_user']) && $_SESSION['Logged_user'] === true): ?>
         <div class="dropdown">
-            <a href="Homepage.php">
+            <a href="Entreprises.php#messagerie">
                 <img src="Pictures/letterPicture.png" alt="letterPicture" style="cursor: pointer;">
             </a>
-            </div>
+        </div>
+        <?php endif; ?>
+
+        <!-- Connexion / Inscription -->
         <div class="dropdown">
             <a>
                 <img src="Pictures/pictureProfil.png" alt="pictureProfil" style="cursor: pointer;">
             </a>
             <div class="dropdown-content">
-                <?php if (isset($_SESSION['Logged_user']) && $_SESSION['Logged_user'] === true): ?>
-                    <!-- Options pour les utilisateurs connectés -->
-                    <a href="#">Mon Profil</a>
-                    <a href="Deconnexion.php">Déconnexion</a>
-                <?php else: ?>
-                    <!-- Options pour les utilisateurs non connectés -->
-                    <a href="Connexion/Form1_connexion.php#modal">Connexion</a>
-                    <a href="Inscription/Form1_inscription.php#modal">S'inscrire</a>
-                <?php endif; ?>
+            <?php if (isset($_SESSION['Logged_user']) && $_SESSION['Logged_user'] === true): ?>
+                <!-- Options pour les utilisateurs connectés -->
+                <?php 
+                if ($_SESSION['role'] == 'Medecin') {
+                    echo "<h1 style='font-size: 18px; text-align: center;'>Dr " . htmlspecialchars($_SESSION['Nom'], ENT_QUOTES, 'UTF-8') . "</h1>";
+                } elseif ($_SESSION['role'] == 'Entreprise') {
+                    echo "<h1 style='font-size: 18px; text-align: center;'>" . htmlspecialchars($_SESSION['Nom'], ENT_QUOTES, 'UTF-8') . "®</h1>";
+                } else {
+                    echo "<h1 style='font-size: 18px; text-align: center;'>" . htmlspecialchars($_SESSION['Nom'], ENT_QUOTES, 'UTF-8') . "</h1>";
+                }
+                ?>
+                <a href="#">Mon Profil</a>
+                <a href="Deconnexion.php">Déconnexion</a>
+            <?php else: ?>
+                <!-- Options pour les utilisateurs non connectés -->
+                <a href="Connexion/Form1_connexion.php#modal">Connexion</a>
+                <a href="Inscription/Form1_inscription.php#modal">S'inscrire</a>
+            <?php endif; ?>
             </div>
+        </div>
+    </div>
+
+    <!-- Message Success -->
+    <?php 
+    if (isset($_SESSION['SuccessCode'])): 
+        SuccesEditor($_SESSION['SuccessCode']);
+        unset($_SESSION['SuccessCode']); // Nettoyage après affichage
+    endif; 
+    ?>
+
+    <!-- Message Erreur -->
+    <?php 
+    if (isset($_SESSION['ErrorCode'])): 
+        ErrorEditor($_SESSION['ErrorCode']);
+        unset($_SESSION['ErrorCode']); // Nettoyage après affichage
+    endif; 
+    ?>
+    
+    <!-- Messagerie -->
+    <div id="messagerie" class="messagerie">
+        <div class="messagerie-content">
+            <!-- Lien de fermeture qui redirige vers Homepage.php -->
+            <a href="/projet-website/Entreprises.php" class="close-btn">&times;</a>
+            <h1>Centre de notifications</h1>
+            <!-- Contenu de la messagerie -->
+            <?php Affiche_notif($_SESSION['Id_user'])?>
         </div>
     </div>
 
