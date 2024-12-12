6286cd1c41c8fac78b3918ca4f5e4478ac867f78
angeliqux <angelique.vella@hotmail.com>
2024-12-12 23:15:06 +0100

    Fix delete users issus

--- Admin/Supprimer_utilisateur.php
+++ Admin/Supprimer_utilisateur.php
@@ -1,6 +1,7 @@
 <?php
 // Inclure la connexion à la base de données
-include("../Fonctions.php");
+include_once '../Notifications/fonction_notif.php';
+include_once ("../Fonctions.php");
 $conn = Connexion_base();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -10,11 +11,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     try {
         // Déterminer la table selon le rôle
         if ($role === 'Patient') {
-            $query = "DELETE FROM patients WHERE Id_Patient = :id";
+            $query = "DELETE FROM PATIENTS WHERE Id_Patient = :id";
         } elseif ($role === 'Medecin') {
-            $query = "DELETE FROM medecins WHERE Id_medecin = :id";
+            $query = "DELETE FROM MEDECINS WHERE Id_medecin = :id";
         } elseif ($role === 'Entreprise') {
-            $query = "DELETE FROM entreprises WHERE Id_entreprise = :id";
+            $query = "DELETE FROM ENTREPRISES WHERE Id_entreprise = :id";
         } else {
             throw new Exception("Rôle invalide.");
         }
@@ -28,8 +29,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <html>
         <head>
             <title>Utilisateur supprimé</title>
-            <link rel=\"stylesheet\" href='website.css'>
-            <link rel=\"stylesheet\" href='navigationBar.css'>
+            <link rel=\"stylesheet\" href='../website.css'>
+            <link rel=\"stylesheet\" href='../navigationBar.css'>
+            <link rel=\"stylesheet\" href='../Notifications/Notifications_style.css'>
+            <link rel=\"stylesheet\" href='Admin.css'>
             <style>
                 body {
                     background-color: turquoise;
@@ -73,42 +76,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <!-- Code de la barre de navigation -->
         <div class=\"navbar\">
             <div id=\"logo\">
-                <a href=\"../Homepage.php\">
-                    <img src=\"../Pictures/logo.png\" alt=\"minilogo\" class=\"minilogo\">
+                <a href=../\"Homepage.php\">
+                    <img src=../\"Pictures/logo.png\" alt=\"minilogo\" class=\"minilogo\">
                 </a>
             </div>
-            <a href=\"../Essais.php\" class=\"nav-btn\">Essais Cliniques</a>
-            <a href=\"../Entreprises.php\" class=\"nav-btn\">Entreprise</a>
-            <a href=\"Contact.php\" class=\"nav-btn\">Contact</a>
-            <div class=\"dropdown\">
-                <a href=\"Homepage.php\">
-                    <img src=\"Pictures/letterPicture.png\" alt=\"letterPicture\" style=\"cursor: pointer;\">
-                </a>
-            </div>
+            <a href=../\"Essais.php\" class=\"nav-btn\">Essais Cliniques</a>
+
+            <!-- Accès à la page de Gestion -->
+            <a href=\"Home_Admin.php\" class=\"nav-btn\">Gestion</a>
+
+            <!-- Connexion / Inscription -->
             <div class=\"dropdown\">
                 <a>
-                    <img src=\"../Pictures/pictureProfil.png\" alt=\"pictureProfil\" style=\"cursor: pointer;\">
+                    <img src=../\"Pictures/pictureProfil.png\" alt=\"pictureProfil\" style=\"cursor: pointer;\">
                 </a>
-                <div class=\"dropdown-content\">
-                <?php if (isset(\$_SESSION['Logged_user']) && \$_SESSION['Logged_user'] === true) { ?>
-                    <?php 
-                    if (\$_SESSION['role'] == 'Medecin') {
-                        echo \"<h1 style='font-size: 18px; text-align: center;'>Dr \" . htmlspecialchars(\$_SESSION['Nom'], ENT_QUOTES, 'UTF-8') . \"</h1>\";
-                    } elseif (\$_SESSION['role'] == 'Entreprise') {
-                        echo \"<h1 style='font-size: 18px; text-align: center;'>\" . htmlspecialchars(\$_SESSION['Nom'], ENT_QUOTES, 'UTF-8') . \"®</h1>\";
-                    } else {
-                        echo \"<h1 style='font-size: 18px; text-align: center;'>\" . htmlspecialchars(\$_SESSION['Nom'], ENT_QUOTES, 'UTF-8') . \"</h1>\";
-                    }
-                    ?>
-                    <a href=\"#\">Mon Profil</a>
-                    <a href=\"../Deconnexion.php\">Déconnexion</a>
-                <?php } else { ?>
-                    <a href=\"../Connexion/Form1_connexion.php#modal\">Connexion</a>
-                    <a href=\"../Inscription/Form1_inscription.php#modal\">S'inscrire</a>
-                <?php } ?>
                 </div>
             </div>
         </div>
+        
         <div class='message-container'>
             <h1>L'utilisateur a été supprimé avec succès</h1>
             <button onclick=\"window.location.href='Home_Admin.php'\">Retour à la page d'accueil</button>
